f3647f8c2e08d353541cd9e4564c927e07ec4289
noori <rajilabana@gmail.com>
2023-02-04 12:06:27 +0000

    added submit button and set an api key

--- script.js
+++ script.js
@@ -13,4 +13,28 @@ function currentWeatherCard(city, weather){
   var humidity = weather.main.humidity;
   var icon = 'https://openweathermap.org/img/w/' + weather.weather[0].icon + '.png';
   var iconDescription = weather.weather[0].description || weather[0].main;
+
+
+
+  var card = $('<div>');
+  var cardText = $('<div>');
+  var heading = $('<h2>');
+  var weatherIcon = $('<img>');
+  var temp = $('<p>');
+  var wind = $('<p>');
+  var humidity = $('<p>');
+
 }
+$('#search-form').on('submit', function(event) {
+    event.preventDefault();
+   const city = $('#search-input').val();
+   console.log(city)
+   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=da7f4aab324ce68fae9d4a040baf0bf0" 
+   
+$.ajax({
+    url: queryURL,
+    method: 'Get'
+}).then(function(data){
+  console.log(data)
+})
+});
