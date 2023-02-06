b87e087e7aa50a785d7388eab9a1ee91acf686b1
noori <rajilabana@gmail.com>
2023-02-06 16:50:06 +0000

    added attributrs class

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ var searchBtn =$("#search-button")
 
 function currentWeatherCard(city, weather){
   var day = moment().format("D/M/YYYY");
-  var temp = weather.main.temp;
+  var tempDg = weather.main.temp;
   var windSpeed = weather.wind.speed;
-  var humidity = weather.main.humidity;
+  var humidityTemp = weather.main.humidity;
   var icon = 'https://openweathermap.org/img/w/' + weather.weather[0].icon + '.png';
   var iconDescription = weather.weather[0].description || weather[0].main;
 
 
-
+  var col=$('<div>');
   var card = $('<div>');
   var cardText = $('<div>');
   var heading = $('<h2>');
@@ -24,7 +24,29 @@ function currentWeatherCard(city, weather){
   var wind = $('<p>');
   var humidity = $('<p>');
 
+// added attributes class with name and values
+  card.attr('class', 'card');
+  cardText.attr('class', 'card-body');
+  card.append(cardText);
+  heading.attr('class', 'h3 card-title');
+  temp.attr('class', 'card-text');
+  wind.attr('class', 'card-text');
+  humidity.attr('class', 'card-text');
+  heading.text(city + ' (' + day + ')');
+  weatherIcon.attr('src', icon);
+  weatherIcon.attr('alt', iconDescription);
+  weatherIcon.attr('class', 'weather-img');
+  heading.append(weatherIcon);
+  temp.text('Temp: ' + tempDg + ' °C');
+  wind.text('Wind: ' + windSpeed + ' KPH');
+  humidity.text('Humidity: ' + humidityTemp + '%');
+  cardText.append(heading, temp, wind, humidity);
+  todayContainer.html('');
+  todayContainer.append(card);
+
 }
+
+
 $('#search-form').on('submit', function(event) {
     event.preventDefault();
    const city = $('#search-input').val();
