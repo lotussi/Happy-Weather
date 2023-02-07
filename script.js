56689b7533c825834a89864d15dcf53d61f8602a
noori <rajilabana@gmail.com>
2023-02-07 12:44:27 +0000

    fixed the error about number of days display on browser

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 
+
+
+
+// var city =document.querySelector('.weather_city');
+// var day = document.querySelector('.weather_day');
+// var tempreature = document.querySelector('.weather_indicator--tempreature>.value');
+// var wind = document.querySelector('.weather_indicator--wind>.value');
+// var humidity = document.querySelector('.weather_indicator--wind.value');
+// var image = document.querySelector('.weather_image');
+
+
+
+
 var searchForm = $('#search-form');
 var searchInput = $('#search-input');
 var todayContainer = $('#today');
 var forecastContainer = $('#forecast');
 var searchHistoryContainer = $('#history');
 var searchBtn =$("#search-button")
-// add function to get city weather and date
+
 function currentWeatherCard(city, weather){
   var day = moment().format("D/M/YYYY");
   var tempDg = weather.main.temp;
@@ -13,7 +26,6 @@ function currentWeatherCard(city, weather){
   var humidityTemp = weather.main.humidity;
   var icon = 'https://openweathermap.org/img/w/' + weather.weather[0].icon + '.png';
   var iconDescription = weather.weather[0].description || weather[0].main;
-
 
   var col=$('<div>');
   var card = $('<div>');
@@ -23,8 +35,8 @@ function currentWeatherCard(city, weather){
   var temp = $('<p>');
   var wind = $('<p>');
   var humidity = $('<p>');
-
-// added attributes class with name and values
+  
+  
   card.attr('class', 'card');
   cardText.attr('class', 'card-body');
   card.append(cardText);
@@ -48,41 +60,43 @@ function currentWeatherCard(city, weather){
 function renderForecastCard(forecast) {
   // variables for data from api
   var icon =
-  'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
-var iconDescription = forecast.weather[0].description;
-var tempDg = forecast.main.temp;
-var humidityTemp = forecast.main.humidity;
-var windSpeed = forecast.wind.speed;
-// Create elements for a card
-var col = $('<div>');
-var card = $('<div>');
-var cardText = $('<div>');
-var cardTitle = $('<h5>');
-var weatherIcon = $('<img>');
-var temp = $('<p>');
-var wind = $('<p>');
-var humidity = $('<p>');
-col.append(card);
-card.append(cardText);
-cardText.append(cardTitle, weatherIcon, temp, wind, humidity);
-col.attr('class', 'col-md');
-col.addClass('five-day-card');
-card.attr('class', 'card bg-primary h-100 text-white');
-cardText.attr('class', 'card-body p-2');
-cardTitle.attr('class', 'card-title');
-temp.attr('class', 'card-text');
-wind.attr('class', 'card-text');
-humidity.attr('class', 'card-text');
-// Add content to elements
-cardTitle.text(moment(forecast.dt_txt).format('D/M/YYYY'));
-weatherIcon.attr('src', icon);
-weatherIcon.attr('alt', iconDescription);
-temp.text('Temp: ' + tempDg + ' °C');
-wind.text('Wind: ' + windSpeed + ' KPH');
-humidity.text('Humidity: ' + humidityTemp + ' %');
-forecastContainer.append(col);
+    'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png';
+  var iconDescription = forecast.weather[0].description;
+  var tempDg = forecast.main.temp;
+  var humidityTemp = forecast.main.humidity;
+  var windSpeed = forecast.wind.speed;
+  // Create elements for a card
+  var col = $('<div>');
+  var card = $('<div>');
+  var cardText = $('<div>');
+  var cardTitle = $('<h5>');
+  var weatherIcon = $('<img>');
+  var temp = $('<p>');
+  var wind = $('<p>');
+  var humidity = $('<p>');
+  col.append(card);
+  card.append(cardText);
+  cardText.append(cardTitle, weatherIcon, temp, wind, humidity);
+  col.attr('class', 'col-md');
+  col.addClass('five-day-card');
+  card.attr('class', 'card bg-primary h-100 text-white');
+  cardText.attr('class', 'card-body p-2');
+  cardTitle.attr('class', 'card-title');
+  temp.attr('class', 'card-text');
+  wind.attr('class', 'card-text');
+  humidity.attr('class', 'card-text');
+  // Add content to elements
+  cardTitle.text(moment(forecast.dt_txt).format('D/M/YYYY'));
+  weatherIcon.attr('src', icon);
+  weatherIcon.attr('alt', iconDescription);
+  temp.text('Temp: ' + tempDg + ' °C');
+  wind.text('Wind: ' + windSpeed + ' KPH');
+  humidity.text('Humidity: ' + humidityTemp + ' %');
+  forecastContainer.append(col);
 }
-// setting a function to get 5 days forcast
+
+
+
 function renderForecast(dailyForecast) {
   var headingCol = $('<div>');
   var heading = $('<h4>');
@@ -92,7 +106,7 @@ function renderForecast(dailyForecast) {
   forecastContainer.html('');
   forecastContainer.append(headingCol);
   function noonTime(forecast) {
-    return forecast.dt_txt.includes('12');
+    return forecast.dt_txt.includes('5');
   }
   var futureCast = dailyForecast.filter(noonTime);
   console.log(futureCast);
@@ -100,24 +114,30 @@ function renderForecast(dailyForecast) {
     renderForecastCard(futureCast[i]);
   }
 }
+
+
+
 function renderItems(city, data) {
   console.log(data)
   console.log(data.list[1])
   currentWeatherCard(city, data.list[0]);
   renderForecast(data.list);
 }
-
 $('#search-form').on('submit', function(event) {
     event.preventDefault();
    var city = $('#search-input').val();
    console.log(city)
-   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=da7f4aab324ce68fae9d4a040baf0bf0" 
+   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=da7f4aab324ce68fae9d4a040baf0bf0" 
    
 $.ajax({
     url: queryURL,
     method: 'Get'
-}).then(function(data){
-renderItems(city, data)
-console.log(data)
+})
+//.then(function (res) {
+ // return res;
+//})
+.then(function(data){
+  renderItems(city, data)
+  console.log(data)
 })
 });
